ad7baee3a7e623b97006b12dbcd5607fc9d8eace
Ka2taka.japan <44127759+ka2taka@users.noreply.github.com>
2020-09-13 23:45:18 +0900

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -72,11 +72,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-#html_theme = 'bizstyle'
-#html_theme_options = {
-#    "rightsidebar": "true"
-#}
 
 html_theme = 'sphinx_rtd_theme'
 
